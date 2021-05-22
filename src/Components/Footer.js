ae26fe4267ee6ef1f2ea87805430e6a46b45b67b
Dann Anthony Astillero <daja@Danns-MacBook-Pro.local>
2021-05-22 11:23:21 +0800

    chore: modified website link

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -22,8 +22,8 @@ const Footer = ({ data }) => {
           <ul className="copyright">
             <li>
               Made by{" "}
-              <a title="PAPA" href="http://www.papareact.com/">
-                PAPA
+              <a title="DAJA" href="https://github.com/DAJAKMPM">
+                DAJA
               </a>
             </li>
           </ul>
